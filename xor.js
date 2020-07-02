52c2092e58de69a7ba1665ab72fcdafbfb79aef8
jessicareynolds <reynolds.jessann@gmail.com>
2020-07-02 19:37:11 -0400

    Code for xor function

--- xor.js
+++ xor.js
@@ -1,5 +1,14 @@
 function xor(cond1, cond2) {
 	// if cond1 is true and cond2 is false return true
+	if ( cond1 && !cond2 ) { 
+		return true 
+		} 
+		else if ( !cond1 && cond2 ) { 
+			return true 
+		}
+		else { 
+		return false 
+		}
 
 	// if cond1 is false and cond2 is true return true
 
