399a9e2cfa7cbebdb783ccde05a90cc87492f875
jessicareynolds <reynolds.jessann@gmail.com>
2020-07-02 20:28:56 -0400

    Add code for loop array

--- all.js
+++ all.js
@@ -1,6 +1,21 @@
+const array = [true, false]
+
 function all(arr) {
+	
+	for( let x = 0; x < arr.length; x++ ) {
+		if ( !arr[ x ]) { 
+			return false }
+	
+	else if ( x >= arr.length ) {
+			return true
+
+		}
+		return arr
+	}
 	// Loop through every item in the array
 	// If the item evaluates to false, return false
 
 	// If the loop ends, return true
 }
+
+console.log(all(array))
