a5b75548a8932bf8b6c65b7698935c47618df4fd
User <user@Users-MacBook-Pro.local>
2020-07-07 23:38:31 -0400

    Add code for dropRight function

--- dropRight.js
+++ dropRight.js
@@ -1,12 +1,14 @@
 function dropRight(arr , n) {
 	// If n is larger than array length return []
+	let newArr = []
+		// Use n to count in from the end to get the index
 	if ( n >= arr.length) {
 		return []
 	}
-
-	arr.slice(0, (arr.length - n))
-	// Use n to count in from the end to get the index
-	return arr.slice(0, (arr.length -n))
-
-	// Return a slice of the array using the index as the end
+		// Return a slice of the array using the index as the end
+	else {
+		newArr = arr.slice(0,(arr.length - n))
+	}
+	return newArr
 }
+console.log(dropRight([1,2,3,4], 2))
