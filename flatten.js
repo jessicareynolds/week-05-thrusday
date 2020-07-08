6563233ba4eee890b1ae8cc588b31148f0b93458
User <user@Users-MacBook-Pro.local>
2020-07-08 00:25:03 -0400

    Update flatten function

--- flatten.js
+++ flatten.js
@@ -1,9 +1,17 @@
 function flatten(arr) {
-	// Create a new array
+	let ret = []
 
+	for (let x = 0; x < arr.length; i++) {
+		if (Array.isArray(arr[x])) {
+			ret = ret.concat(arr[x])
+		} else {
+			ret.push(arr[x])
+		}
+	}
+	return ret
+}
+	// Create a new array
 	// Loop through every item in the array
 	// If the item is an array, add its items to the new array
 	// If not add the item itself to the new array
-
 	// return the new array
-}
